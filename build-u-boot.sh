80da2dc03cd0526a12079dfeab4f16f8eb988066
Rokoucha <6058487+rokoucha@users.noreply.github.com>
2024-03-02 19:52:22 +0900

    fix u-boot build command

--- build-u-boot.sh
+++ build-u-boot.sh
@@ -10,7 +10,7 @@ pushd ./u-boot
 git switch -d v2024.01
 git apply ../patches/u-boot/*.patch
 
-make ARCH="${ARCH}" CROSS_COMPILE="${CROSS_COMPILE}"  distclean
+make ARCH="${ARCH}" CROSS_COMPILE="${CROSS_COMPILE}" distclean
 make ARCH="${ARCH}" CROSS_COMPILE="${CROSS_COMPILE}" am335x-akiduki-dg_defconfig
 make ARCH="${ARCH}" CROSS_COMPILE="${CROSS_COMPILE}" -j4
 
